9bf160210278d66ad50fb5f4ef3fe5f90cae57f9
Mikhail Morozov <mikhail.morozov@team.wrike.com>
2020-05-23 13:34:15 +0300

    profile widget and some fixes for quiz

---
+++ lib/src/widgets/profile.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:vockify/src/redux/actions/unauthorize_action.dart';
+import 'package:vockify/src/redux/state/app_state.dart';
+import 'package:vockify/src/redux/state/user_state.dart';
+import 'package:vockify/src/vockify_colors.dart';
+import 'package:vockify/src/widgets/app_layout.dart';
+
+class ProfileWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppLayoutWidget(
+      title: 'PROFILE',
+      profile: false,
+      body: Center(
+        child: StoreConnector<AppState, UserState>(
+          distinct: true,
+          converter: (store) => store.state.user,
+          builder: (context, user) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(fit: BoxFit.fill, image: NetworkImage(user.avatar)),
+                  ),
+                ),
+                ListTile(
+                  title: Center(
+                    child: Text(
+                      '${user.firstName} ${user.lastName}',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ),
+                  subtitle: Text(
+                    user.email,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                FlatButton(
+                  color: VockifyColors.lightSteelBlue,
+                  onPressed: () {
+                    final store = StoreProvider.of<AppState>(context);
+                    store.dispatch(UnauthorizeAction());
+                  },
+                  child: Text('LOGOUT'),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
